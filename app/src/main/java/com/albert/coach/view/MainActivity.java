4eac83e778add2df75722e9b1c09f65257672258
Albert-Ubun <dorcealbertmary@gmail.com>
2019-06-24 19:38:28 -0400

    Resizen my Button calculate LinearLayout layout_heigth = march_parent

--- app/src/main/java/com/albert/coach/view/MainActivity.java
+++ app/src/main/java/com/albert/coach/view/MainActivity.java
@@ -2,10 +2,13 @@ package com.albert.coach.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.albert.coach.R;
 import com.albert.coach.controller.Control;
@@ -35,7 +38,21 @@ public class MainActivity extends AppCompatActivity {
         btnrd = (RadioButton)findViewById(R.id.btnMan);
 
         lbResult = (TextView)findViewById(R.id.lbResult);
+        listenBtn();
 
+    }
+
+    /**
+     * Listen calculation button
+     */
+    private void listenBtn()
+    {
+        ((Button) findViewById(R.id.btnCalc)).setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"test",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
