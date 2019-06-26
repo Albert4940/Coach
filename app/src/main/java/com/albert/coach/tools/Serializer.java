f9f1a13942ca8b981e280403a486ad3357fa8e38
Albert-Ubun <dorcealbertmary@gmail.com>
2019-06-25 20:45:23 -0400

    I write a serializer methode in my class Serializer

--- app/src/main/java/com/albert/coach/tools/Serializer.java
+++ app/src/main/java/com/albert/coach/tools/Serializer.java
@@ -1,5 +1,41 @@
 package com.albert.coach.tools;
 
+import android.content.Context;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public abstract class Serializer {
-    
+
+    /**
+     *Serialization an object
+     * @param filename
+     * @param object
+     * @param context
+     */
+    public static void serialize(String filename, Object object, Context context)
+    {
+
+        try
+        {
+            FileOutputStream file = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            ObjectOutputStream oos;
+            try
+            {
+                oos = new ObjectOutputStream(file);
+                oos.writeObject(object);
+                oos.flush();
+                oos.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
